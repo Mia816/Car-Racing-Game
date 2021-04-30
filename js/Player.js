fceb4b7aaca72563189fa914bf183a0723cbbd0f
Mia816 <71109850+Mia816@users.noreply.github.com>
2021-04-29 18:58:09 -0700

    Update Player.js

--- js/Player.js
+++ js/Player.js
@@ -3,6 +3,7 @@ class Player {
     this.index = null;
     this.distance = 0;
     this.name = null;
+    this.rank = null;
   }
 
   getCount(){
@@ -37,7 +38,18 @@ class Player {
     database.ref('/').update({
       players: null
     })
-
-
   }
+
+  getCarsAtEnd(){
+    database.ref('carsAtEnd').on("value",(data)=>{
+      this.rank = data.val();
+    });
+  }
+
+  static updateCarsAtEnd(rank){
+    database.ref('/').update({
+      carsAtEnd: rank
+    })
+  }
+
 }
